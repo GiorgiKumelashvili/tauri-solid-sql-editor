9bc455f2075ff73655e0afef7500e338c04fa886
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2022-08-01 17:15:32 +0400

    added custom title bar but I think its unneccessary on mac or on all platform

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,22 @@ import 'windi.css';
 import { render } from 'solid-js/web';
 import { Router } from 'solid-app-router';
 import App from './app';
-import './app.css'
+import './app.css';
+
+import { appWindow } from '@tauri-apps/api/window';
+
+document.getElementById('titlebar-minimize').addEventListener('click', () => appWindow.minimize());
+document
+  .getElementById('titlebar-maximize')
+  .addEventListener('click', () => appWindow.toggleMaximize());
+document.getElementById('titlebar-close').addEventListener('click', () => appWindow.close());
+
 render(
   () => (
     <Router>
       <App />
     </Router>
   ),
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 

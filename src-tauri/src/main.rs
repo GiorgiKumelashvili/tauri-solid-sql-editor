eedeea79ee857075fe2add066eea1ca8ece9a745
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2023-04-01 16:38:41 +0400

    added creating new instance

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,30 +1,28 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 use tauri::Menu;
 
 // Our Tauri Command
 #[tauri::command]
-fn return_string(word: String) -> String{
-    return word
+fn return_string(word: String) -> String {
+    return word;
 }
 
 #[tauri::command]
 fn my_custom_command() -> String {
-  println!("I was invoked from JS!");
-  return "asd".to_string();
+    println!("I was invoked from JS!");
+    return "asd".to_string();
 }
 
 fn main() {
-  let menu = Menu::new(); // configure the menu
+    let menu = Menu::new(); // configure the menu
 
-  tauri::Builder::default()
-    .menu(menu)
-    .invoke_handler(tauri::generate_handler![return_string, my_custom_command])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .menu(menu)
+        .invoke_handler(tauri::generate_handler![return_string, my_custom_command])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
-
-
--- src/app.tsx
+++ src/app.tsx
@@ -34,16 +34,18 @@ import { BaseDirectory, readTextFile } from '@tauri-apps/api/fs';
 
 import { EditorState, Extension } from '@codemirror/state';
 
-import {
-  closeBrackets,
-  autocompletion,
-  closeBracketsKeymap,
-  completionKeymap,
-} from '@codemirror/autocomplete';
+import { closeBrackets, autocompletion, closeBracketsKeymap, completionKeymap } from '@codemirror/autocomplete';
 
 import { highlightSelectionMatches, searchKeymap } from '@codemirror/search';
 
 import { sql } from '@codemirror/lang-sql';
+import { ask, confirm, message, open, save } from '@tauri-apps/api/dialog';
+import { isPermissionGranted, requestPermission, sendNotification } from '@tauri-apps/api/notification';
+import { getCurrent, getAll } from '@tauri-apps/api/window';
+import { appDir } from '@tauri-apps/api/path';
+import { Command } from '@tauri-apps/api/shell';
+import { WebviewWindow } from '@tauri-apps/api/window';
+// import { WebviewWindow } from '@tauri-apps/api/window';
 
 //! IMPORTANT
 //TODO create custom ui desktop like library in pure css for pure perfomance or tailwind core
@@ -97,13 +99,123 @@ const App: Component = () => {
   });
 
   async function handleClick(event) {
-    // Invoke the command from our backend using the invoke function
-    const result = await invoke('return_string', {
-      word: 'This is the argument',
+    // // Invoke the command from our backend using the invoke function
+    // const result = await invoke('return_string', {
+    //   word: 'This is the argument',
+    // });
+
+    // // print the result to an alert
+    // alert(result);
+
+    // const yes = await ask('Are you sure?', 'Tauri');
+    // const yes2 = await ask('This action cannot be reverted. Are you sure?', { title: 'Tauri', type: 'info' });
+
+    // const confirmed2 = await confirm('This action cannot be reverted. Are you sure?', {
+    //   title: 'Tauri',
+    //   type: 'warning',
+    // });
+
+    // await message('File not found', { title: 'Tauri', type: 'error' });
+
+    // const selected = await open({
+    //   multiple: true,
+    //   filters: [
+    //     {
+    //       name: 'Image',
+    //       extensions: ['png', 'jpeg'],
+    //     },
+    //   ],
+    // });
+    // if (Array.isArray(selected)) {
+    //   // user selected multiple files
+    // } else if (selected === null) {
+    //   // user cancelled the selection
+    // } else {
+    //   // user selected a single file
+    // }
+
+    // // Open a selection dialog for directories
+    // const selected = await open({
+    //   // recursive: true,
+    //   title:'test',
+
+    //   // directory: true,
+    //   // multiple: true,
+    //   defaultPath: await appDir(),
+    // });
+
+    // console.log(selected);
+
+    // const filePath = await save({
+    //   // multiple: true,
+    //   defaultPath: await appDir(),
+    //   filters: [
+    //     {
+    //       name: 'Image',
+    //       extensions: ['svg', 'png']
+    //     },
+    //   ],
+    // });
+
+    // console.log(filePath);
+
+    // let permissionGranted = await isPermissionGranted();
+    // console.log(permissionGranted);
+
+    // if (!permissionGranted) {
+    //   const permission = await requestPermission();
+    //   permissionGranted = permission === 'granted';
+    // }
+    // if (permissionGranted) {
+    //   sendNotification('Tauri is awesome!');
+    //   sendNotification({ title: 'TAURI', body: 'Tauri is awesome!' });
+    // }
+
+    // console.log(getCurrent());
+    // console.log(getAll());
+
+    // window.open('https://youtube.com');
+    // console.log('https://youtube.com');
+    //  await tauri.shell.open('https://example.com');
+
+    // loading embedded asset:
+    const webview = new WebviewWindow('theUniqueLabel-', {
+      url: '../README.md',
+      resizable: false,
+      title: 'Settings',
     });
 
-    // print the result to an alert
-    alert(result);
+    // // alternatively, load a remote URL:
+    // const webview = new WebviewWindow('theUniqueLabel', {
+    //   url: 'https://github.com/tauri-apps/tauri',
+    // });
+
+    webview.once('tauri://created', function() {
+      // webview window successfully created
+      console.log('created');
+    });
+    webview.once('tauri://error', function(e) {
+      // an error happened creating the webview window
+      console.log('error');
+    });
+    const unlisten2 = await webview.onCloseRequested(async event => {
+      console.log(123);
+
+      const confirmed = await confirm('Are you sure?');
+      if (!confirmed) {
+        // user did not confirm closing the window; let's prevent it
+        event.preventDefault();
+      }
+    });
+
+    // you need to call unlisten if your handler goes out of scope e.g. the component is unmounted
+    unlisten2();
+
+    // emit an event to the backend
+    await webview.emit('some event', 'data');
+    // listen to an event from the backend
+    const unlisten = await webview.listen('event name', e => {});
+    unlisten();
   }
 
   async function showFile() {
@@ -117,12 +229,27 @@ const App: Component = () => {
     editorView().setState(newState);
   }
 
+  const newWIndow = () => {
+    console.log(123);
+  };
+
   return (
     <>
       <div>
-        <button onClick={handleClick}>Click Me</button>
+        <button class="btn" onClick={newWIndow}>
+          New window 2
+        </button>
         <br />
-        <button onClick={showFile}>Read file content</button>
+        <br />
+        <button class="btn" onClick={handleClick}>
+          Open native manu
+        </button>
+        <br />
+        <br />
+        <button class="btn" onClick={showFile}>
+          Read file content
+        </button>
+        <br />
         <br />
 
         <div ref={editorRef} />
@@ -182,4 +309,3 @@ export default App;
 // };
 
 // export default App;
-
